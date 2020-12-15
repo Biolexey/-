f79f48e6ada7ebd9368f10ae536775e164f258b9
RevB <biolexey@gmail.com>
2020-12-15 11:32:56 +0900

    12/15

--- 2013/win.py
+++ 2013/win.py
@@ -9,4 +9,42 @@ def print1(ret):
     for txt in ret:
         print(txt)
 
-print1(solve1("txt1.txt"))
+#print1(solve1("txt1.txt"))
+
+def solve2(file):
+    txts = solve1(file)
+    all = set()
+    groups = []
+    for txt in txts:
+        als = txt.split("&")
+        group = []
+        for al in als:
+            group.append(al)
+            all.add(al)
+        group.sort()
+        groups.append(group)
+
+    all2 = sorted(list(all))
+    answer = set()
+    for group in groups:
+        ans = [""]
+        for al in all2:
+            if al in group:
+                for i in range(len(ans)):
+                    ans[i] += "{0}=true ".format(al)
+            else:
+                tmp = len(ans)
+                ans = ans*2
+                for i in range(tmp):
+                    ans[i] += "{0}=true ".format(al)
+                    ans[i+tmp] += "{0}=true ".format(al)
+        for txt in ans:
+            answer.add(txt)
+    if len(answer) > 0:
+        for a in answer:
+            print(a)
+    else:
+        print("none")
+    print(all, all2, groups)
+
+solve2("txt1.txt")
